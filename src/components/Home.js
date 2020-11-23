fb471ea640eb8d3bb9f8f7bb62dfb410d83b287d
Mostafa Kanaan <mos.kan@hotmail.com>
2020-11-24 00:31:22 +0100

    add animation to icons

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,8 +6,18 @@ const openInNewTab = (url) => {
     const newWindow = window.open(url, '_blank', 'noopener,noreferrer')
     if (newWindow) newWindow.opener = null
 }
+
 class Home extends React.Component {
+    constructor() {
+        super();
+        this.state = { FBfade: false, Ifade: false, Efade: false }
+      }
+      
     render() {
+        const FBfade = this.state.FBfade;
+        const Ifade = this.state.Ifade;
+        const Efade = this.state.Efade;
+
         return(
             <div id="home" className='Home'>
             <img src="../favicon.ico" className="App-logo" alt="icon"/>
@@ -15,11 +25,26 @@ class Home extends React.Component {
             Still working on it, trying to come up with a nice design...<i class="fab fa-github-square"></i>
             </p> 
             <div>
-            <span className='clickableIcon' onClick={() => {openInNewTab('https://facebook.com/SteveKanaan/')}}><i class="fab fa-facebook"></i></span>  
-            <span className='clickableIcon' onClick={() => {openInNewTab('https://instagram.com/_dermusti/')}}><i class="fab fa-instagram"></i></span>
-            <span className='clickableIcon' onClick={() => {openInNewTab('mailto:muus.kaan@hotmail.com')}}><i class="far fa-envelope"></i></span>
+            <span onClick={() => this.setState({ FBfade: true })}
+                onAnimationEnd={() => {this.setState({ FBfade: false }); openInNewTab('https://facebook.com/SteveKanaan/')}}
+                className={FBfade ? 'clickableIcon fade' : 'clickableIcon'}
+            >
+                <i className={"fab fa-facebook " + this.state.fade}></i>
+            </span>  
+            <span onClick={() => this.setState({ Ifade: true })}
+                onAnimationEnd={() => {this.setState({ Ifade: false });openInNewTab('https://instagram.com/_dermusti/')}}
+                className={Ifade ? 'clickableIcon fade' : 'clickableIcon'}
+            >
+                <i className="fab fa-instagram"></i>
+            </span>
+            <span onClick={() => this.setState({ Efade: true })}
+                onAnimationEnd={() => {this.setState({ Efade: false });openInNewTab('mailto:muus.kaan@hotmail.com')}}
+                className={Efade ? 'clickableIcon fade' : 'clickableIcon'}
+            >
+                <i className="far fa-envelope"></i>
+            </span>
             </div>
-            <div class="line"></div>
+            <div className="line"></div>
             
             <TypedText
             strings={[
@@ -29,7 +54,6 @@ class Home extends React.Component {
                 ''
             ]} text="My name is Mustafa Kanaan, I'm a "
             />
-
             </div>
             );
         }
